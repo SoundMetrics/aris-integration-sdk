57f7e56b08b1459592a5727f72a654ffa1be8d96
Frank Vasquez <Frank.Vasquez@gmail.com>
2017-07-26 00:17:59 -0700

    Add unit tests for reordering.

--- common/code/UnitTest/unittest.cpp
+++ common/code/UnitTest/unittest.cpp
@@ -1,11 +1,93 @@
 #include "CppUnitTest.h"
+#include "Reorder.h"
 #include "Depth.h"
+#include <vector>
+#include <fstream>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace Aris::AcousticMath;
 
 namespace UnitTest
 {	
+    bool ReorderTest(uint32_t pingMode, uint32_t samplesPerBeam,
+        const char * inputFileName, const char * expectedFileName)
+    {
+        using namespace Aris;
+
+        const auto dataSize = PingModeToNumBeams(pingMode) * samplesPerBeam;
+
+        // Build input frame header
+        ArisFrameHeader header;
+        header.PingMode = pingMode;
+        header.SamplesPerBeam = samplesPerBeam;
+        header.ReorderedSamples = 0;
+
+        // Read unordered image data from input file
+        auto samples = std::vector<uint8_t>(dataSize, 0xAA);
+        std::ifstream inputFile(inputFileName, std::ifstream::binary);
+        inputFile.read((char *)&samples[0], dataSize);
+
+        // Reorder samples 
+        Reorder(header, &samples[0]);
+
+        // Read reordered image data from expected results file
+        auto reorderedData = std::vector<uint8_t>(dataSize, 0xFF);
+        std::ifstream expectedFile(expectedFileName, std::ifstream::binary);
+        expectedFile.read((char *)&reorderedData[0], dataSize);
+
+        // Compare result of reordering with expected data
+        if (memcmp(&samples[0], &reorderedData[0], dataSize) != 0) {
+            return false;
+        }
+
+        // Verify that ReorderedSamplse flag is set after reordering
+        if (header.ReorderedSamples != 1) {
+            return false;
+        }
+
+        return true;
+    }
+
+    TEST_CLASS(ReorderTests)
+    {
+    public:
+
+        TEST_METHOD(PingMode1Freq1200ReorderTest)
+        {
+            bool success = ReorderTest(1, 512, "../../data/input_pingmode_1_1200.dat", "../../data/expected_pingmode_1_1200.dat");
+            
+            Assert::IsTrue(success);
+        }
+
+        TEST_METHOD(PingMode1Freq1800ReorderTest)
+        {
+            bool success = ReorderTest(1, 544, "../../data/input_pingmode_1_1800.dat", "../../data/expected_pingmode_1_1800.dat");
+
+            Assert::IsTrue(success);
+        }
+
+        TEST_METHOD(PingMode3ReorderTest)
+        {
+            bool success = ReorderTest(3, 512, "../../data/input_pingmode_3.dat", "../../data/expected_pingmode_3.dat");
+
+            Assert::IsTrue(success);
+        }
+
+        TEST_METHOD(PingMode6ReorderTest)
+        {
+            bool success = ReorderTest(6, 410, "../../data/input_pingmode_6.dat", "../../data/expected_pingmode_6.dat");
+
+            Assert::IsTrue(success);
+        }
+
+        TEST_METHOD(PingMode9ReorderTest)
+        {
+            bool success = ReorderTest(9, 512, "../../data/input_pingmode_9.dat", "../../data/expected_pingmode_9.dat");
+
+            Assert::IsTrue(success);
+        }
+
+    };
 
     TEST_CLASS(DepthTests)
     {
@@ -65,6 +147,7 @@ namespace UnitTest
             Assert::AreEqual(actual, expected);
         }
 
+        // salinity of 50 PPT is invalid so treat as salt water
         TEST_METHOD(WrongWaterDepthTest)
         {
             const double pressure = 37.0;
