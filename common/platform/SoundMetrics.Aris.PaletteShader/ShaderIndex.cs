5a426e8dd547eb3d55291720bf29f92482af70bd
Curt Nichols <curt.nichols@live.com>
2018-05-03 11:08:48 -0700

    Add ArisPaletteShader

---
+++ common/platform/SoundMetrics.Aris.PaletteShader/ShaderIndex.cs
@@ -0,0 +1,23 @@
+﻿// Copyright 2010-2018 Sound Metrics Corp. All Rights Reserved.
+
+namespace SoundMetrics.Aris.PaletteShader
+{
+    /// <summary>
+    /// The pixel shader effect applied to the image displayed by the sonar image control.
+    /// Applies to <see cref="ArisPaletteShader.ShaderIndex"/>.
+    /// </summary>
+    public enum ShaderIndex
+    {
+        /// <summary> None ( Default) </summary>
+        NoEffect,
+
+        /// <summary> Smoothing  (Noise reduction) </summary>
+        Smooth,
+
+        /// <summary> Sharpening filter </summary>
+        Sharpen,
+
+        /// <summary> Edge detect ( Disabled in the UI for now) </summary>
+        Edges,
+    }
+}
