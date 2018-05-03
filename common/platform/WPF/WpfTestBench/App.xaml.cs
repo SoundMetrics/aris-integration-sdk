143a359b38d2a321d5be5733fcc42173273dc428
Curt Nichols <curt.nichols@live.com>
2018-05-03 10:40:19 -0700

    Add skeletal WPF test bench

---
+++ common/platform/WPF/WpfTestBench/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using Serilog;
+
+namespace WpfTestBench
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ConfigureLogging();
+        }
+
+        private static void ConfigureLogging()
+        {
+            Log.Logger = new LoggerConfiguration()
+                            .WriteTo.Trace(outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+                            .CreateLogger();
+        }
+    }
+}
