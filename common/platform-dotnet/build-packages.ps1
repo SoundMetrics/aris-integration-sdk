5e2fee7797bc81bf4ca8919a311c3f4f6868b49b
Curt Nichols <curt.nichols@live.com>
2018-06-25 15:40:08 -0700

    Remove dead code.

--- common/platform-dotnet/build-packages.ps1
+++ common/platform-dotnet/build-packages.ps1
@@ -5,15 +5,6 @@ Param(
 # Print the script name
 "Script: $MyInvocation.MyCommand.Name"
 
-<#
-@ECHO OFF
-REM ---------------------------------------------------------------------------
-REM Builds the file utility nuget packages.
-REM ---------------------------------------------------------------------------
-
-// /p:PackageVersion=1.0.13-alpha /p:Version=1.0.13.55555
-#>
-
 $package_version = Get-Content "ver.platform.txt"
 '$package_version=' + $package_version
 
@@ -43,29 +34,3 @@ Foreach ($el in $assemblies) {
     '---------------------------------------------------------------------'
     dotnet pack -c Release /p:Version=$split_version /p:PackageVersion=$package_version $el
 }
-<#
-SETLOCAL
-
-SET BUILD_NUMBER=%1
-IF "%BUILD_NUMBER%" == "" SET BUILD_NUMBER=55555
-ECHO BUILD_NUMBER=%BUILD_NUMBER%
-
-SET /p VERSION=<ver.platform.txt
-
-ECHO Making packages for %VERSION%.%BUILD_NUMBER%
-
-SET PACKCMD=dotnet pack -c Release /p:Version=%VERSION%
-ECHO PACKCMD=%PACKCMD%
-
-SET PROJECTS=SoundMetrics.Aris.Comms SoundMetrics.Aris.Config SoundMetrics.Aris.FrameHeaderInjection
-SET PROJECTS=%PROJECTS% SoundMetrics.Aris.Messages SoundMetrics.Aris.ReorderCS SoundMetrics.NativeMemory
-
-FOR %%p in (%PROJECTS%) do (
-    ECHO ---------------------------------------------------------------------
-    ECHO %%p
-    ECHO ---------------------------------------------------------------------
-    %PACKCMD% %%p
-)
-
-ENDLOCAL
-#>
