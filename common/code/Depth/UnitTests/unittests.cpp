c46670cf4aeeeddf8abe996a03bf56b9c9eeada9
Curt Nichols <curt.nichols@live.com>
2018-07-10 10:33:14 -0700

    Simplified depth code; code gen reduced > 90% (#90)

--- common/code/Depth/UnitTests/unittests.cpp
+++ common/code/Depth/UnitTests/unittests.cpp
@@ -10,8 +10,8 @@ namespace UnitTests
     {
     private:
 
-        double kAtmosphere = 14.6959;
-        double kMetersPerPSI = 0.702398;
+        const double kAtmosphere = 14.6959;
+        const double kMetersPerPSI = 0.702398;
 
     public:
 
@@ -20,7 +20,7 @@ namespace UnitTests
             const double pressure = 20.0;
             const uint32_t salinity = 0;
             const double waterTemp = 15;
-            const double freshWaterDensity15C = 0.999;
+            const double freshWaterDensity15C = 0.999f;
             const double expected = (pressure - kAtmosphere) * kMetersPerPSI / freshWaterDensity15C;
             const double actual = CalculateDepthM(pressure, salinity, waterTemp);
 
@@ -32,7 +32,7 @@ namespace UnitTests
             const double pressure = 32.0;
             const uint32_t salinity = 15;
             const double waterTemp = 15;
-            const double brackishWaterDensity15C = 1.011;
+            const double brackishWaterDensity15C = 1.011f;
             const double expected = (pressure - kAtmosphere) * kMetersPerPSI / brackishWaterDensity15C;
             const double actual = CalculateDepthM(pressure, salinity, waterTemp);
 
@@ -44,7 +44,7 @@ namespace UnitTests
             const double pressure = 65.0;
             const uint32_t salinity = 35;
             const double waterTemp = 15;
-            const double saltWaterDensity15C = 1.026;
+            const double saltWaterDensity15C = 1.026f;
             const double expected = (pressure - kAtmosphere) * kMetersPerPSI / saltWaterDensity15C;
             const double actual = CalculateDepthM(pressure, salinity, waterTemp);
 
@@ -56,7 +56,7 @@ namespace UnitTests
             const double pressure = 23.0;
             const uint32_t salinity = 35;
             const double waterTemp = 40;
-            const double saltWaterDensity30C = 1.022;
+            const double saltWaterDensity30C = 1.022f;
             const double expected = (pressure - kAtmosphere) * kMetersPerPSI / saltWaterDensity30C;
             const double actual = CalculateDepthM(pressure, salinity, waterTemp);
 
@@ -69,11 +69,35 @@ namespace UnitTests
             const double pressure = 37.0;
             const uint32_t salinity = 50;
             const double waterTemp = 10;
-            const double saltWaterDensity10C = 1.027;
+            const double saltWaterDensity10C = 1.027f;
             const double expected = (pressure - kAtmosphere) * kMetersPerPSI / saltWaterDensity10C;
             const double actual = CalculateDepthM(pressure, salinity, waterTemp);
 
             Assert::AreEqual(actual, expected);
+        }
+
+        TEST_METHOD(LowTempDepthTest)
+        {
+          const double pressure = 65.0;
+          const uint32_t salinity = 35;
+          const double waterTemp = -2;
+          const double saltWaterDensity0C = 1.028f;
+          const double expected = (pressure - kAtmosphere) * kMetersPerPSI / saltWaterDensity0C;
+          const double actual = CalculateDepthM(pressure, salinity, waterTemp);
+
+          Assert::AreEqual(actual, expected);
+        }
+
+        TEST_METHOD(HighTempDepthTest)
+        {
+          const double pressure = 65.0;
+          const uint32_t salinity = 35;
+          const double waterTemp = 31;
+          const double saltWaterDensity30C = 1.022f;
+          const double expected = (pressure - kAtmosphere) * kMetersPerPSI / saltWaterDensity30C;
+          const double actual = CalculateDepthM(pressure, salinity, waterTemp);
+
+          Assert::AreEqual(actual, expected);
         }
     };
 }
