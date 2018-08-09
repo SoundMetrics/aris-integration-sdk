bc94a7156125d37c93c61564573b1c5f1237d023
Curt Nichols <curt.nichols@live.com>
2018-08-09 13:53:45 -0700

    Update protoc path

--- tools/arislog/arislog/generate-protobuf-files.cmd
+++ tools/arislog/arislog/generate-protobuf-files.cmd
@@ -6,7 +6,10 @@ IF EXIST generated del/q generated\*.pb.*
 IF NOT EXIST generated mkdir generated
 
 set PROTO_FILES_DIR=..\..\..\common\protobuf
-set PROTOC_DIR=..\..\..\submodules\vcpkg\installed\x86-windows-static\tools
+set PROTOC_DIR=..\..\..\submodules\vcpkg\packages\protobuf_x86-windows-static\tools\protobuf
+
+cd
+dir %PROTOC_DIR%
 
 REM wildcard doesn't seem to work any more, so loop through the files.
 
