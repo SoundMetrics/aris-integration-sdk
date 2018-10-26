3421acc7ad87a227de3bc427d682a236d8c5eb65
Curt Nichols <curt.nichols@live.com>
2018-10-26 08:09:35 -0700

    Updated nuget packages (excluding protobuf-related)
    Removed main.fs from SoundMetrics.Aris.Comms.UT, as it appeared no longer necessary to fake out the compiler.

--- common/platform-dotnet/test/SoundMetrics.Aris.Comms.UT/Main.fs
+++
@@ -1,6 +0,0 @@
-﻿module Main
-
-// This is here solely to remove a warning/error during compilation.
-// error FS0988: Main module of program is empty: nothing will happen when it is run
-
-let [<EntryPoint>] main _argv = 0
