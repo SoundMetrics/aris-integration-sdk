3488b78b7baca2601c9bdddbcd9f1d3d415c323f
Curt Nichols <curt.nichols@live.com>
2018-12-28 11:02:27 -0800

    Add assembly info

---
+++ common/platform-dotnet/SoundMetrics.Data/AssemblyInfo.fs
@@ -0,0 +1,9 @@
+﻿namespace SoundMetrics.Data
+
+[<assembly: System.Reflection.AssemblyCompanyAttribute("Sound Metrics Corporation")>]
+[<assembly: System.Reflection.AssemblyConfigurationAttribute("Release")>]
+[<assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright (c) 2014-2018 Sound Metrics Corporation")>]
+[<assembly: System.Reflection.AssemblyProductAttribute("Basic Data")>]
+[<assembly: System.Reflection.AssemblyTitleAttribute("SoundMetrics.Data")>]
+
+do()
