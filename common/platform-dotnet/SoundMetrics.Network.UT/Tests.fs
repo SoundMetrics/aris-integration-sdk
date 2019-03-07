480a18c8cf1451bceca47922f02fcdf2dfc5784a
Curt Nichols <curt.nichols@live.com>
2019-03-07 08:16:57 -0800

    Add SoundMetrics.Network.UT

---
+++ common/platform-dotnet/SoundMetrics.Network.UT/Tests.fs
@@ -0,0 +1,11 @@
+namespace SoundMetrics.Network.UT
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type TestClass () =
+
+    [<TestMethod>]
+    member this.TestMethodPassing () =
+        Assert.IsTrue(false);
