72232020b17135917cced25fc276ddfa9fa71571
Curt Nichols <curt.nichols@live.com>
2019-04-12 13:22:34 -0700

    Minimal unit test

---
+++ common/platform-dotnet/SoundMetrics.Aris.AcousticSettings.UT/AcousticSettingsRaw.fs
@@ -0,0 +1,29 @@
+namespace SoundMetrics.Aris.AcousticSettings.UT
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open SoundMetrics.Aris.AcousticSettings
+
+module Details =
+
+    let areEqual<'T> a b = Assert.AreEqual<'T>(a, b)
+    let areNotEqual<'T> a b = Assert.AreNotEqual<'T>(a, b)
+
+open Details
+
+[<TestClass>]
+type AcousticSettingsRawTest () =
+
+    [<TestMethod>]
+    member __.``Equality`` () =
+
+        let a = AcousticSettingsRaw.Invalid
+        let b = AcousticSettingsRaw.Invalid
+        areEqual a b
+
+    [<TestMethod>]
+    member __.``Inequality`` () =
+
+        // Not terribly complete
+        let a = AcousticSettingsRaw.Invalid
+        let b = { AcousticSettingsRaw.Invalid with SampleCount = a.SampleCount + 1u }
+        areNotEqual a b
---
+++ common/platform-dotnet/SoundMetrics.Aris.AcousticSettings/ISettingsStore.fs
@@ -0,0 +1,12 @@
+﻿// Copyright 2019 Sound Metrics Corp. All Rights Reserved.
+
+namespace SoundMetrics.Aris.AcousticSettings.Store
+
+type ArisCollectiveSettings =
+
+    member __.A = 42
+
+
+type IArisSettingsStore =
+    abstract AddOrUpdate : key: uint32 -> bool
+    abstract Remove : key: uint32 -> bool
