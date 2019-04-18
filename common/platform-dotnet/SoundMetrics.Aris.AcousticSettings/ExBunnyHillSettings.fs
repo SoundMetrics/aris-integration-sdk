5774c4c5a055a13ade795303518221c4d70e8cdc
Curt Nichols <curt.nichols@live.com>
2019-04-18 14:25:34 -0700

    Add experimental new settings work; initial steps...

---
+++ common/platform-dotnet/SoundMetrics.Aris.AcousticSettings/ExBunnyHillSettings.fs
@@ -0,0 +1,45 @@
+﻿// Copyright 2014-2019 Sound Metrics Corp. All Rights Reserved.
+
+namespace SoundMetrics.Aris.AcousticSettings.Experimental
+
+type Range = float32
+
+type BunnyHillSettings = {
+    WindowStart:    Range
+    WindowEnd:      Range
+}
+
+type BunnyHillChange =
+    | ChangeWindowStart of value: Range
+    | ChangeWindowEnd of value: Range
+
+module BunnyHill =
+
+    [<CompiledName("BunnyHillProjection")>]
+    let bunnyHillProjection =
+
+        let toSettings externalContext (bunnyHill : BunnyHillSettings) =
+            { PingMode = 12 }
+
+        let change bunnyHill change : BunnyHillSettings =
+
+            match change with
+            | ChangeWindowStart value ->
+                { bunnyHill with WindowStart = value }
+            | ChangeWindowEnd value ->
+                { bunnyHill with WindowEnd = value }
+
+        let minWindowStart = 0.7f
+        let maxWindowEnd = 20.0f
+
+        let constrain (bunnyHill : BunnyHillSettings) =
+            let minmax f = max minWindowStart (min maxWindowEnd f)
+            let struct (windowStart, windowEnd) =
+                let struct (s, e) =
+                    struct (minmax bunnyHill.WindowStart, minmax bunnyHill.WindowEnd)
+                struct (min s e, max s e)
+            { WindowStart = windowStart; WindowEnd = windowEnd }
+
+        { ToSettings = toSettings
+          Constrain = constrain
+          Change = change }
---
+++ common/platform-dotnet/SoundMetrics.Aris.AcousticSettings/ExSettingsStructure.fs
@@ -0,0 +1,62 @@
+﻿// Copyright 2014-2019 Sound Metrics Corp. All Rights Reserved.
+
+namespace SoundMetrics.Aris.AcousticSettings.Experimental
+
+type DeviceSettings = {
+    PingMode:   int
+}
+
+type AuxLensType = None | Telephoto
+type Salinity = Fresh = 0 | Brackish = 15 | Saltwater = 35
+
+type ExternalContext = {
+    WaterTemp:  float32
+    AuxLens:    AuxLensType
+    Salinity:   Salinity
+}
+
+type ProjectionToSettings<'P> = ExternalContext -> 'P -> DeviceSettings
+
+type ProjectionMap<'P,'C> = {
+    Constrain:  'P -> 'P
+    Change:     'P -> 'C -> 'P
+    ToSettings: ProjectionToSettings<'P>
+}
+
+//-----------------------------------------------------------------------------
+
+type ComputedValues = {
+    Resolution:     float32
+    AutoFocusRange: float32
+    SoundSpeed:     float32
+}
+
+
+module SettingsProjections =
+
+    let private getComputedValues extCtx (deviceSettings: DeviceSettings) =
+        // TODO
+        { Resolution = 6.9f; AutoFocusRange = 4.0f; SoundSpeed = 1500.0f }
+
+    //-----------------------------------------------------------------------------
+
+    let private normalize (settings : DeviceSettings) =
+
+        failwith "nyi"
+
+    //-----------------------------------------------------------------------------
+
+    [<CompiledName("ChangeProjection")>]
+    let changeProjection<'P,'C> (pmap: ProjectionMap<'P,'C>)
+                                (projection: 'P)
+                                (changes: 'C list)
+                                externalContext
+                                : struct ('P * DeviceSettings * ComputedValues) =
+
+        let constrainedProjection =
+            changes |> List.fold pmap.Change projection // Apply any changes
+                    |> pmap.Constrain
+        let deviceSettings = pmap.ToSettings externalContext constrainedProjection
+                                |> normalize
+        let computedValues = deviceSettings |> getComputedValues externalContext
+        struct (constrainedProjection, deviceSettings, computedValues)
