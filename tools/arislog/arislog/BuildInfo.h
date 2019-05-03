468b0e38f2adbb00f2a0c19f62432df670cb575d
Curt Nichols <curt.nichols@live.com>
2019-05-03 12:50:10 -0700

    Add version via resource file

---
+++ tools/arislog/arislog/BuildInfo.h
@@ -0,0 +1,27 @@
+// ResourceCompiler
+// 1.0.1.55555
+//
+// [SemanticVersion] = [1.0.1] 
+// [MajorMinorRev] = [1.0.1] 
+// [BuildNumber] = [55555] 
+// [Thumbprint] = [] 
+// [Namespace] = [] 
+// [BuildDate] = [3 May 2019] 
+// [RcFileVersion] = [1,0,1,55555] 
+// [RcFileVersionString] = [1.0.1.55555] 
+// [RcProductVersion] = [1,0,1,55555] 
+// [RcProductVersionString] = [1.0.1.55555] 
+// [AssemblyVersion] = [1.0.1.55555] 
+// [AssemblyFileVersion] = [1.0.1.55555] 
+
+//-------------------------------------------------------------------------------------------------
+// WARNING: !!! This file contains only automatically generated content. Do not modify it. !!!
+//-------------------------------------------------------------------------------------------------
+
+#define FILE_VERSION 1,0,1,55555
+#define FILE_VERSION_STRING "1.0.1.55555"
+#define PRODUCT_VERSION 1,0,1,55555
+#define PRODUCT_VERSION_STRING "1.0.1.55555"
+#define BUILD_NUMBER "55555"
+#define VCS_THUMBPRINT ""
+
