9c860a4697e1572e65f7c1d5dff0e74b6c142aea
Curt Nichols <curt.nichols@live.com>
2019-05-31 09:51:47 -0700

    Trying to sort out what broke nuget on the build server.

--- common/platform-dotnet/add-local-packages.ps1
+++ common/platform-dotnet/add-local-packages.ps1
@@ -13,9 +13,9 @@ $local_nuget_source = '\\build\SMC-Nuget-Packages'
 Get-ChildItem $package_folder -Filter *.nupkg |
 Foreach-Object {
 
-    $package_path = $_.FullName
+    $package_path = Get-Item $_ | Resolve-Path -Relative
     '$package_path=' + $package_path
 
-    .\.nuget\nuget delete "$package_name" "$package_version" -NonInteractive -Source $local_nuget_source
+    .\.nuget\nuget delete "$package_name" "$package_version" -NonInteractive -NoPrompt -Source $local_nuget_source
     .\.nuget\nuget add $package_path -Source $local_nuget_source -NonInteractive
 }
