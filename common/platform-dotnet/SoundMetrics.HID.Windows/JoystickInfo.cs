7a1a3c9a25b6e2f53ec8f6fef3daeb036caf194b
Curt Nichols <curt.nichols@live.com>
2019-08-09 14:29:16 -0700

    Extract JoystickInfo

---
+++ common/platform-dotnet/SoundMetrics.HID.Windows/JoystickInfo.cs
@@ -0,0 +1,20 @@
+﻿// Copyright 2014-2019 Sound Metrics Corp. All Rights Reserved.
+
+using System;
+
+namespace SoundMetrics.HID.Windows
+{
+    public static partial class Joystick
+    {
+        public struct JoystickInfo
+        {
+            public uint JoystickId;
+            public JoyCaps Caps;
+
+            private static Lazy<JoystickInfo> empty =
+                new Lazy<JoystickInfo>(() => new JoystickInfo());
+
+            public static JoystickInfo Empty = empty.Value;
+        }
+    }
+}
