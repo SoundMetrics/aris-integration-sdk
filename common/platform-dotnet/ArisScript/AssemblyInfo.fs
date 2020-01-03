d00947a5dba9dca7e27294f1c83b995a62af4680
Curt Nichols <curt.nichols@live.com>
2020-01-02 16:09:53 -0800

    naming

--- common/platform-dotnet/ArisScript/AssemblyInfo.fs
+++ common/platform-dotnet/ArisScript/AssemblyInfo.fs
@@ -1,4 +1,4 @@
-﻿namespace VoyagerFocusTest.AssemblyInfo
+﻿namespace ArisScript.AssemblyInfo
 
 open System.Reflection
 open System.Runtime.CompilerServices
@@ -7,12 +7,12 @@ open System.Runtime.InteropServices
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[<assembly: AssemblyTitle("VoyagerFocusTest")>]
+[<assembly: AssemblyTitle("ArisScript")>]
 [<assembly: AssemblyDescription("")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("VoyagerFocusTest")>]
-[<assembly: AssemblyCopyright("Copyright ©  2018")>]
+[<assembly: AssemblyProduct("ArisScript")>]
+[<assembly: AssemblyCopyright("Copyright ©  2018-2019")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
 
