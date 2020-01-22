77a1d58c22c5f77fa8f215fb4bdaa3c03eaf37d7
Curt Nichols <curt.nichols@live.com>
2020-01-22 13:33:28 -0800

    Add failing unit test to allow setting up the new projects in the build.

---
+++ common/platform-dotnet/SoundMetrics.Aris.SimplifiedProtocol.UT/FramePartHeaderTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SoundMetrics.Aris.SimplifiedProtocol.UT
+{
+    [TestClass]
+    public class FramePartHeaderTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Fail("Start by failing in order to test the build system.");
+        }
+    }
+}
