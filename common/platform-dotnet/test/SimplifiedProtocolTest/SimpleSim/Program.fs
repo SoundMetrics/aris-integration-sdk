cc5af131cb9a8a19243cca068970fe6d5fd5c4a8
Curt Nichols <curt.nichols@live.com>
2020-03-20 17:34:09 -0700

    Add command line args for SimpleSim

--- common/platform-dotnet/test/SimplifiedProtocolTest/SimpleSim/Program.fs
+++ common/platform-dotnet/test/SimplifiedProtocolTest/SimpleSim/Program.fs
@@ -1,8 +1,32 @@
 ﻿// Learn more about F# at http://fsharp.org
 
+open Argu
+open SimpleSim
 open System
+open System.IO
+
+[<AutoOpen>]
+module internal MainDetails =
+
+    let printWelcomeMessage (sink : TextWriter) =
+        sink.WriteLine("SimpleSim - simulator for ARIS Simplified Protocol")
+
+    let getUsage () =
+        ArgumentParser
+            .Create<ProgramArguments>(programName = "simplesim.exe")
+            .PrintUsage()
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+
+    printWelcomeMessage Console.Out
+
+    match ProgramArguments.GetProgramArguments argv with
+    | Arguments args -> ()
+    | Message message ->
+        Console.Out.WriteLine(message)
+        getUsage() |> Console.Out.WriteLine
+    | ShowUsage ->
+        getUsage() |> Console.Out.WriteLine
+
     0 // return an integer exit code
