61f7741e3e21e04229f759ae46e1d00bf3f4731f
Curt Nichols <curt.nichols@live.com>
2020-04-02 07:23:46 -0700

    Add noise test for UnweightedMedianFilter

--- common/platform-dotnet/SoundMetrics.Data.UT/UnweightedMedianFilterTests.cs
+++ common/platform-dotnet/SoundMetrics.Data.UT/UnweightedMedianFilterTests.cs
@@ -51,5 +51,43 @@ namespace SoundMetrics.Data.Filters.UT
                 ++caseIndex;
             }
         }
+
+        [TestMethod]
+        public void TestNoise()
+        {
+            var filter = new Filter(5);
+
+            // Median filters aren't magic, but they do have some staying power for
+            // keeping a good signal.
+            var cases = new[]
+            {
+                new { Input = 10, IsBufferFilled = false, ExpectedValue = 10 },
+                new { Input = 10, IsBufferFilled = false, ExpectedValue = 10 },
+                new { Input = 10, IsBufferFilled = false, ExpectedValue = 10 },
+                new { Input = 10, IsBufferFilled = false, ExpectedValue = 10 },
+                new { Input = 10, IsBufferFilled = true, ExpectedValue = 10 },
+
+                new { Input = -100, IsBufferFilled = true, ExpectedValue = 10 },
+                new { Input = -100, IsBufferFilled = true, ExpectedValue = 10 },
+                new { Input = +100, IsBufferFilled = true, ExpectedValue = 10 },
+                new { Input = +100, IsBufferFilled = true, ExpectedValue = 10 },
+            };
+
+            int caseIndex = 0;
+
+            foreach (var c in cases)
+            {
+                var message = $"[{caseIndex}] Input={c.Input}; IsBufferFilled={c.IsBufferFilled}; ExpectedValue={c.ExpectedValue}";
+                var expected = c.ExpectedValue;
+
+                int actual;
+                bool isBufferFilled = filter.AddValue(c.Input, out actual);
+
+                Assert.AreEqual(c.IsBufferFilled, isBufferFilled, message);
+                Assert.AreEqual(expected, actual, message);
+
+                ++caseIndex;
+            }
+        }
     }
 }
