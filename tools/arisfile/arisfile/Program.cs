ef794afccec8c39878e29dc280e76c3bc7ad1111
Curt Nichols <curt.nichols@live.com>
2020-04-03 14:12:56 -0700

    Initial structure for arisfile

---
+++ tools/arisfile/arisfile/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace arisfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("arisfile - ARIS recorded file analysis");
+        }
+    }
+}
