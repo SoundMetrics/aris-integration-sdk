9cb60a6b00a5649eb28468474e8ceae7109abd28
Curt Nichols <curt.nichols@live.com>
2020-04-03 15:25:20 -0700

    Add arisfile.analysis

---
+++ tools/arisfile/arisfile.analysis/Analysis.cs
@@ -0,0 +1,13 @@
+﻿using Serilog;
+using System.IO;
+
+namespace arisfile.analysis
+{
+    public static class Analysis
+    {
+        public static void Run(Stream stream)
+        {
+            Log.Warning("Analysis.Run is not yet implemented.");
+        }
+    }
+}
--- tools/arisfile/arisfile/Program.cs
+++ tools/arisfile/arisfile/Program.cs
@@ -1,12 +1,58 @@
-﻿using System;
+﻿using arisfile.analysis;
+using CommandLine;
+using Serilog;
+using System.IO;
 
 namespace arisfile
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("arisfile - ARIS recorded file analysis");
+            const string LoggingTemplate =
+                @"{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}";
+
+            Log.Logger =
+                new LoggerConfiguration()
+#if DEBUG
+                    .MinimumLevel.Debug()
+#endif
+                    .WriteTo.Console(outputTemplate: LoggingTemplate)
+                    .CreateLogger();
+
+            try
+            {
+                PerformAnalysis(args);
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
+
+        private static void PerformAnalysis(string[] args)
+        {
+            Log.Information("arisfile - ARIS recorded file analysis");
+
+            Parser.Default.ParseArguments<ProgramOptions>(args)
+                .WithParsed<ProgramOptions>(options =>
+                {
+                    try
+                    {
+                        using (var stream = OpenArisFile(options.FilePath))
+                        {
+                            Log.Information("Opened '{FilePath}'.", options.FilePath);
+
+                            Analysis.Run(stream);
+                        }
+                    }
+                    catch (IOException ex)
+                    {
+                        Log.Error(ex, "An error occurred.");
+                    }
+                });
+        }
+
+        private static Stream OpenArisFile(string path) => File.OpenRead(path);
     }
 }
