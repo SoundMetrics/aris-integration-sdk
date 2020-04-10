fc92fb1d02c0b789d906bfac95485c868ae41b2d
Curt Nichols <curt.nichols@live.com>
2020-04-10 10:38:30 -0700

    Remove boost::noncopyable from FrameStream common code

--- common/code/FrameStream/FrameBuilder.h
+++ common/code/FrameStream/FrameBuilder.h
@@ -1,17 +1,17 @@
 // The MIT License (MIT)
-// 
+//
 // Copyright (c) 2013-2017 Sound Metrics Corporation. All Rights Reserved.
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,20 +23,21 @@
 #pragma once
 
 #include <boost/asio/buffer.hpp>
-#include <boost/utility.hpp>
 #include <vector>
 #include <stdint.h>
 
 namespace Aris {
 namespace Network {
 
-class FrameBuilder : boost::noncopyable {
+class FrameBuilder {
 public:
   FrameBuilder(int frameIndex, boost::asio::const_buffer header,
                boost::asio::const_buffer firstDataFragment,
                size_t totalDataSize);
+  FrameBuilder(const FrameBuilder& other) = delete;
   FrameBuilder(FrameBuilder &&other);
 
+  FrameBuilder& operator=(const FrameBuilder&) = delete;
   FrameBuilder &operator=(FrameBuilder &&other);
 
   int FrameIndex() const { return frameIndex; }
@@ -63,10 +64,6 @@ private:
 
   void ClearOtherInstance(FrameBuilder &other);
   void ValidateInputs();
-
-  // Unavailable:
-  FrameBuilder(const FrameBuilder &);
-  FrameBuilder &operator=(const FrameBuilder &);
 };
 }
 }
--- common/code/FrameStream/SlidingWindowFrameAssembler.h
+++ common/code/FrameStream/SlidingWindowFrameAssembler.h
@@ -1,17 +1,17 @@
 // The MIT License (MIT)
-// 
+//
 // Copyright (c) 2013-2017 Sound Metrics Corporation. All Rights Reserved.
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -32,11 +32,14 @@ namespace Network {
 // Assembles frames from multiple packets. The term "finished" means we've
 // stopped assembling packets; the frame may yet be incomplete. The term
 // "complete" means we've received all the packets for the frame.
-class SlidingWindowFrameAssembler : boost::noncopyable {
+class SlidingWindowFrameAssembler {
 public:
   SlidingWindowFrameAssembler(
       std::function<void(int, int)> sendAck,
       std::function<void(FrameBuilder &)> onFrameFinished);
+
+  SlidingWindowFrameAssembler(const SlidingWindowFrameAssembler&) = delete;
+  SlidingWindowFrameAssembler& operator=(const SlidingWindowFrameAssembler&) = delete;
 
   void ProcessPacket(boost::asio::const_buffer data);
   void Flush();
