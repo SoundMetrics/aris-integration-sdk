69c1f16f285ce65ee513794be704b18aa36cd647
Curt Nichols <curt.nichols@live.com>
2020-07-14 10:42:01 -0700

    Move CollectionFile to its own source file

--- common/platform-dotnet/Samples/CollectRecordingStats/Collection.cs
+++ common/platform-dotnet/Samples/CollectRecordingStats/Collection.cs
@@ -1,86 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
 namespace CollectRecordingStats
 {
-    public class CollectionFile
-    {
-        public string Path { get; private set; }
-        public long FileLength { get; private set; }
-        public long FrameCount { get; private set; }
-        public DateTime? FirstFrameSonarTimestamp { get; private set; }
-        public DateTime? LastFrameSonarTimestamp { get; private set; }
-        public TimeSpan? AverageFramePeriod { get; private set; }
-        public ulong? FirstGoTime { get; private set; }
-        public ulong? LastGoTime { get; private set; }
-        public double? AverageGoTimePeriodMicros { get; private set; }
-        public string ErrorMessage { get; private set; }
-
-        public CollectionFile(
-            string path,
-            long fileLength,
-            long frameCount,
-            DateTime? firstFrameSonarTimestamp,
-            DateTime? lastFrameSonarTimestamp,
-            ulong? firstGoTime,
-            ulong? lastGoTime)
-            : this(path)
-        {
-            if (frameCount < 0)
-            {
-                throw new ArgumentOutOfRangeException(nameof(frameCount));
-            }
-
-            Path = path;
-            FileLength = fileLength;
-            FrameCount = frameCount;
-            FirstFrameSonarTimestamp = firstFrameSonarTimestamp;
-            LastFrameSonarTimestamp = lastFrameSonarTimestamp;
-            FirstGoTime = firstGoTime;
-            LastGoTime = lastGoTime;
-            ErrorMessage = "";
-
-            // Calculate averages when it makes sense.
-            if (frameCount > 1)
-            {
-                var intervals = frameCount - 1;
-
-                if (firstFrameSonarTimestamp is DateTime firstSonarTS
-                    && lastFrameSonarTimestamp is DateTime lastSonarTS)
-                {
-                    AverageFramePeriod = (lastSonarTS - firstSonarTS) / intervals;
-                }
-
-                if (firstGoTime is ulong firstGo && lastGoTime is ulong lastGo)
-                {
-                    AverageGoTimePeriodMicros = (double)(lastGo - firstGo) / intervals;
-                }
-            }
-        }
-
-        public CollectionFile(string path, string errorMessage)
-            : this(path)
-        {
-            if (string.IsNullOrWhiteSpace(errorMessage))
-            {
-                throw new ArgumentException("Missing error message");
-            }
-
-            ErrorMessage = errorMessage;
-        }
-
-        private CollectionFile(string path)
-        {
-            if (string.IsNullOrWhiteSpace(path))
-            {
-                throw new ArgumentNullException(nameof(path));
-            }
-
-            Path = path;
-        }
-    }
 
     public class CollectionResult
     {
