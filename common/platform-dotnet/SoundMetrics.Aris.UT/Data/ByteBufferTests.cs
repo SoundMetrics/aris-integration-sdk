62a89c5863944d93e24c17331e39f524d1d9ba44
Curt Nichols <curt.nichols@live.com>
2020-07-15 17:52:50 -0700

    Add ByteBuffer

---
+++ common/platform-dotnet/SoundMetrics.Aris.UT/Data/ByteBufferTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SoundMetrics.Aris.Data;
+using System;
+
+namespace SoundMetrics.Aris.UT.Data
+{
+    [TestClass]
+    public sealed class ByteBufferTests
+    {
+        private void InitializeTo42(Span<byte> buffer)
+        {
+            InitializeTo(buffer, 42);
+        }
+
+        private void InitializeTo(Span<byte> buffer, byte value)
+        {
+            for (int index = 0; index < buffer.Length; ++index)
+            {
+                buffer[index] = value;
+            }
+        }
+
+        [TestMethod]
+        public void NegativeSizedBuffer()
+        {
+            _ = Assert.ThrowsException<ArgumentOutOfRangeException>(
+                () => new ByteBuffer(-1, InitializeTo42));
+        }
+
+        [TestMethod]
+        public void EmptyBuffer()
+        {
+            var buffer = new ByteBuffer(0, InitializeTo42);
+            Assert.AreEqual(0, buffer.Length);
+            Assert.AreEqual(0, buffer.Span.Length);
+        }
+
+        [TestMethod]
+        public void SingletonBuffer()
+        {
+            var expected = (byte)(new Random().Next(0, 255));
+            var buffer = new ByteBuffer(1, buf => InitializeTo(buf, expected));
+            var actual = buffer.Span[0];
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Transform()
+        {
+            void TransformFn(ReadOnlySpan<byte> inputBuffer, Span<byte> outputBuffer)
+            {
+                for (int index = 0; index < inputBuffer.Length; ++index)
+                {
+                    outputBuffer[index] = (byte)(2 * inputBuffer[index]);
+                }
+            }
+
+            var expected = 42 * 2;
+            var buffer1 = new ByteBuffer(8, InitializeTo42);
+            var buffer2 = buffer1.Transform(TransformFn);
+
+            Assert.IsNotNull(buffer2);
+            Assert.AreEqual(buffer1.Length, buffer2.Length);
+
+            for (int index = 0; index < buffer2.Length; ++index)
+            {
+                Assert.AreEqual(
+                    expected,
+                    buffer2.Span[index],
+                    $"Unexpected value at index {index}");
+            }
+        }
+    }
+}
---
+++ common/platform-dotnet/SoundMetrics.Aris/Data/ByteBuffer.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Runtime.InteropServices;
+
+namespace SoundMetrics.Aris.Data
+{
+    /// <summary>
+    /// Implements a buffer in native heap so it doesn't live
+    /// on the Large Object Heap (LOH).
+    /// </summary>
+    public sealed class ByteBuffer : SafeHandleZeroOrMinusOneIsInvalid
+    {
+        public delegate void InitializeBuffer(Span<byte> buffer);
+        public delegate void TransformBuffer(
+            ReadOnlySpan<byte> inputBuffer, Span<byte> outputBuffer);
+
+        public ByteBuffer(int length, InitializeBuffer initializeBuffer)
+            : base(ownsHandle: true)
+        {
+            if (length < 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(length),
+                    $"{nameof(length)} must be greater than zero");
+            }
+
+            this.length = length;
+            base.SetHandle(Marshal.AllocHGlobal(length));
+
+            unsafe
+            {
+                var writeableBuffer =
+                    new Span<byte>(DangerousGetHandle().ToPointer(), length);
+                initializeBuffer(writeableBuffer);
+            }
+        }
+
+        protected override bool ReleaseHandle()
+        {
+            Marshal.FreeHGlobal(DangerousGetHandle());
+            return true;
+        }
+
+        public int Length => length;
+
+        public ReadOnlySpan<byte> Span
+        {
+            get
+            {
+                unsafe
+                {
+                    return new ReadOnlySpan<byte>(
+                        DangerousGetHandle().ToPointer(), length);
+                }
+            }
+        }
+
+        public ByteBuffer Transform(TransformBuffer transformBuffer)
+        {
+            void initialize(Span<byte> output) => transformBuffer(this.Span, output);
+
+            var newBuffer = new ByteBuffer(length, initialize);
+            return newBuffer;
+        }
+
+        private readonly int length;
+    }
+}
