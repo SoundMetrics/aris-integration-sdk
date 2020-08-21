7470120a38edc41db5174922b6d2a41a18d61312
Curt Nichols <curt.nichols@live.com>
2020-08-21 12:03:34 -0700

    Now pulling settings from the command line

--- common/platform-dotnet/UnnamedTestProgram/Program.cs
+++ common/platform-dotnet/UnnamedTestProgram/Program.cs
@@ -30,14 +30,9 @@ namespace UnnamedTestProgram
 
         private static void RunTest(TestOptions options)
         {
-            if (options.SerialNumber is null)
-            {
-                Log.Error("No serial number given (--serial-number)");
-                return;
-            }
-
             Log.Information($"Test device {options.SerialNumber}.");
             Log.Information($"Test duration, {options.Duration} minute(s).");
+            Log.Information($"Test settings [{options.Settings}]");
 
             if (options.Duration is uint minutesDuration)
             {
@@ -54,36 +49,9 @@ namespace UnnamedTestProgram
                         iscancelling = OnCancel(e, cts, iscancelling);
                     };
 
-                    // Apply acoustic settings: cookie = 16
-                    // frame_period = 0
-                    // samples_per_channel = 1000 sample_start_delay = 930 cycle_period = 2329
-                    // beam_sample_period = 22 pulse_width = 20 enable_xmit = 0 frequency_select = 1 system_type =
+                    var rawSettingsCommand = CreateSettingsCommand(options);
 
-                    /*
-                        13.9, 1000, 930, 23290, 22, 20, 1, true, high, 1, 12.0
-                     */
-                    var rawSettings = new RawSettings
-                    {
-                        FrameRate = 13.9f,
-                        SamplesPerBeam = 1000,
-                        SampleStartDelay = 930,
-                        CyclePeriod = 2329 * 10,
-                        SamplePeriod = 22,
-                        PulseWidth = 20,
-                        PingMode = 1,
-                        EnableTransmit = true,
-                        Frequency = RawSettingsFrequency.High,
-                        Enable150Volts = true,
-                        ReceiverGain = 12.0f,
-                    };
-
-                    rawSettings = RawSettings.Deserialize("13.9, 1000, 930, 23290, 22, 20, 1, true, high, 1, 12.0");
-
-                    var rawSettingsCommand = new PassthroughSettings(
-                        new[] { "#raw" }
-                            .Concat(rawSettings.Serialize()));
-
-                    Log.Information("Sending command:");
+                    Log.Information("Created command:");
                     foreach (var line in rawSettingsCommand.GenerateCommand())
                     {
                         Log.Information($"[{line}]");
@@ -141,6 +109,16 @@ namespace UnnamedTestProgram
                 }
 
                 return true;
+            }
+
+            static PassthroughSettings CreateSettingsCommand(TestOptions options)
+            {
+                var rawSettings = RawSettings.Deserialize(options.Settings);
+
+                var rawSettingsCommand = new PassthroughSettings(
+                    new[] { "#raw" }
+                        .Concat(rawSettings.Serialize()));
+                return rawSettingsCommand;
             }
         }
 
