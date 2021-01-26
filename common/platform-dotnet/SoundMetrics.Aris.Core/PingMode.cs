88b553e4ddd1ff13f1b00297d28c55f965558a7a
Curt Nichols <curt.nichols@live.com>
2021-01-26 06:28:00 -0800

    Add SoundMetrics.Aris.Core for dotnet core 2.0 support (.NET Framework 4.7+ compatible)

---
+++ common/platform-dotnet/SoundMetrics.Aris.Core/PingMode.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoundMetrics.Aris.Core
+{
+    public struct PingMode
+    {
+        private PingMode(int integralValue, bool isValid)
+        {
+            this.integralValue = integralValue;
+            this.isValid = isValid;
+        }
+
+        internal int IntegralValue
+        {
+            get
+            {
+                if (isValid)
+                {
+                    return integralValue;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(
+                        "Invalid integral ping mode",
+                        nameof(IntegralValue));
+                }
+            }
+        }
+
+        public static readonly PingMode PingMode1 = new PingMode(1, isValid: true);
+        public static readonly PingMode PingMode3 = new PingMode(3, isValid: true);
+        public static readonly PingMode PingMode6 = new PingMode(6, isValid: true);
+        public static readonly PingMode PingMode9 = new PingMode(9, isValid: true);
+
+        public static PingMode From(int integralValue)
+        {
+            // Parsing invalid integral values as a valid value allows us to
+            // manipulate headers with invalid values.
+            var isValid = IsValidIntegralValue(integralValue);
+            return new PingMode(integralValue, isValid);
+        }
+
+        public static PingMode Invalid(int integralValue)
+        {
+            if (IsValidIntegralValue(integralValue))
+            {
+                throw new ArgumentOutOfRangeException($"{integralValue} is a valid integral value");
+            }
+
+            return new PingMode(integralValue, isValid: false);
+        }
+
+        internal bool IsValid => IsValidIntegralValue(this.integralValue);
+
+        private static bool IsValidIntegralValue(int integralValue) =>
+            ValidIntegralValues.Contains(integralValue);
+
+        private readonly int integralValue;
+        private readonly bool isValid;
+
+        private static readonly HashSet<int> ValidIntegralValues = new HashSet<int>(new[] { 1, 3, 6, 9 });
+    }
+}
---
+++ common/platform-dotnet/SoundMetrics.Aris.Core/README.md
@@ -0,0 +1,8 @@
+﻿# README
+
+## Purpose
+
+This project maintains a cross-platform set of types and code that can be
+shared with .NET Framework 4.7/4.8. Specifically, this project is a
+.NET Standard 2.0 library, which is compatible with the above as well as
+.NET Core implementations.
