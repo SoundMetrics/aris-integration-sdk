51208317a01ed936a0cafb8dde8c0b26b385c260
Curt Nichols <curt.nichols@live.com>
2021-02-03 08:53:55 -0800

    Add ARIS default acoustic settings

---
+++ common/platform-dotnet/SoundMetrics.Aris.Core/Raw/AcousticSettingsRawSerializable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SoundMetrics.Aris.Core.Raw
+{
+    public sealed class AcousticSettingsRawSerializable
+    {
+
+        public static AcousticSettingsRawSerializable From(AcousticSettingsRaw settings)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AcousticSettingsRaw ToRaw()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
