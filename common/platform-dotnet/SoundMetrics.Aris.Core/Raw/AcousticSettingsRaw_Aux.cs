657a3ee0349840aa81c0eda012fd4d1555d61821
Curt Nichols <curt.nichols@live.com>
2022-05-25 13:44:47 -0700

    Extract DistancePerTempRatio

--- common/platform-dotnet/SoundMetrics.Aris.Core/Raw/AcousticSettingsRaw_Aux.cs
+++ common/platform-dotnet/SoundMetrics.Aris.Core/Raw/AcousticSettingsRaw_Aux.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2010-2022 Sound Metrics Corp.
+﻿// Copyright (c) 2010-2022 Sound Metrics Corp. All Rights Reserved.
 
 using System;
 using System.Collections.Generic;
@@ -140,31 +140,6 @@ namespace SoundMetrics.Aris.Core.Raw
             {
                 throw new ArgumentException($"Unhandled system type: [{systemType}]");
             }
-        }
-
-        // Unfortunately C# doesn't deal with units as a first class concept.
-        // So here's a shim.
-        private struct DistancePerTempRatio
-        {
-            public DistancePerTempRatio(Distance distance) : this(distance, (Temperature)1) { }
-
-            public DistancePerTempRatio(Distance distance, Temperature temperature)
-            {
-                this.distance = distance;
-                this.temperature = temperature;
-            }
-
-            public static implicit operator DistancePerTempRatio(double d)
-                => new DistancePerTempRatio((Distance)d);
-
-            public static Distance operator *(DistancePerTempRatio dpt, Temperature t)
-                => (t / dpt.temperature) * dpt.distance;
-
-            public static Distance operator *(Temperature t, DistancePerTempRatio dpt)
-                => (t / dpt.temperature) * dpt.distance;
-
-            private readonly Distance distance;
-            private readonly Temperature temperature;
         }
     }
 }
---
+++ common/platform-dotnet/SoundMetrics.Aris.Core/Raw/DistancePerTempRatio.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) 2022 Sound Metrics Corp. All Rights Reserved.
+
+namespace SoundMetrics.Aris.Core.Raw
+{
+    // Unfortunately C# doesn't deal with units as a first class concept.
+    // So here's a shim.
+    internal struct DistancePerTempRatio
+    {
+        public DistancePerTempRatio(Distance distance) : this(distance, (Temperature)1) { }
+
+        public DistancePerTempRatio(Distance distance, Temperature temperature)
+        {
+            this.distance = distance;
+            this.temperature = temperature;
+        }
+
+        public static implicit operator DistancePerTempRatio(double d)
+            => new DistancePerTempRatio((Distance)d);
+
+        public static Distance operator *(DistancePerTempRatio dpt, Temperature t)
+            => (t / dpt.temperature) * dpt.distance;
+
+        public static Distance operator *(Temperature t, DistancePerTempRatio dpt)
+            => (t / dpt.temperature) * dpt.distance;
+
+        private readonly Distance distance;
+        private readonly Temperature temperature;
+    }
+}
