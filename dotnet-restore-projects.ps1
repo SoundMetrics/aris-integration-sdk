55c2c193ec4797e1225302b264447a210c1cdd6b
Curt Nichols <curt.nichols@live.com>
2022-05-26 15:46:01 -0700

    Add script for restoring modern dotnet project packages.
    TeamCity is deprecating certain step runners, and the recent update caught us out.

---
+++ dotnet-restore-projects.ps1
@@ -0,0 +1,14 @@
+# Restore step for modern dotnet projects
+
+dotnet restore submodules\build-tools\BuildInfo\BuildInfo.fsproj
+
+dotnet restore common\platform-dotnet\SoundMetrics.Aris.Messages\SoundMetrics.Aris.Messages.csproj
+dotnet restore common\platform-dotnet\SoundMetrics.Aris.Comms\SoundMetrics.Aris.Comms.fsproj
+dotnet restore common\platform-dotnet\SoundMetrics.Aris.FrameHeaderInjection\SoundMetrics.Aris.FrameHeaderInjection.fsproj
+dotnet restore common\platform-dotnet\SoundMetrics.NativeMemory\SoundMetrics.NativeMemory.fsproj
+dotnet restore common\platform-dotnet\SoundMetrics.Aris.ReorderCS\SoundMetrics.Aris.ReorderCS.csproj
+dotnet restore common\platform-dotnet\SoundMetrics.Scripting\SoundMetrics.Scripting.fsproj
+dotnet restore common\platform-dotnet\SoundMetrics.Common\SoundMetrics.Common.fsproj
+dotnet restore common\platform-dotnet\SoundMetrics.Network\SoundMetrics.Network.fsproj
+dotnet restore common\platform-dotnet\SoundMetrics.Dataflow\SoundMetrics.Dataflow.fsproj
+dotnet restore common\platform-dotnet\SoundMetrics.Data\SoundMetrics.Data.fsproj
