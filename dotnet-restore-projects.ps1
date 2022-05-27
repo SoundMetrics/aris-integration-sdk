e53416c00642aa5efc8599a62564ad129f36b742
Curt Nichols <curt.nichols@live.com>
2022-05-27 10:33:08 -0700

    Build fixes: additional projects to restore and corrections

--- dotnet-restore-projects.ps1
+++ dotnet-restore-projects.ps1
@@ -3,6 +3,7 @@
 dotnet restore submodules\build-tools\BuildInfo\BuildInfo.fsproj
 
 dotnet restore common\platform-dotnet\SoundMetrics.Aris\SoundMetrics.Aris.csproj
+dotnet restore common\platform-dotnet\SoundMetrics.Aris.Core\SoundMetrics.Aris.Core.csproj
 dotnet restore common\platform-dotnet\SoundMetrics.Aris.Messages\SoundMetrics.Aris.Messages.csproj
 dotnet restore common\platform-dotnet\SoundMetrics.Aris.Comms\SoundMetrics.Aris.Comms.fsproj
 dotnet restore common\platform-dotnet\test\SoundMetrics.Aris.Comms.Integration.Test\SoundMetrics.Aris.Comms.Integration.Test.fsproj
@@ -13,11 +14,15 @@ dotnet restore common\platform-dotnet\SoundMetrics.Aris.ReorderCS\SoundMetrics.A
 dotnet restore common\platform-dotnet\SoundMetrics.Scripting\SoundMetrics.Scripting.fsproj
 dotnet restore common\platform-dotnet\SoundMetrics.Common\SoundMetrics.Common.fsproj
 dotnet restore common\platform-dotnet\SoundMetrics.Network\SoundMetrics.Network.fsproj
+dotnet restore common\platform-dotnet\SoundMetrics.Network.UT\SoundMetrics.Network.UT.fsproj
 dotnet restore common\platform-dotnet\SoundMetrics.Dataflow\SoundMetrics.Dataflow.fsproj
 dotnet restore common\platform-dotnet\SoundMetrics.Data\SoundMetrics.Data.fsproj
 dotnet restore common\platform-dotnet\SsdpAdHocTest.Model\SsdpAdHocTest.Model.fsproj
+dotnet restore common\platform-dotnet\SsdpAdHocTestWPF\SsdpAdHocTestWPF.csproj
 dotnet restore common\platform-dotnet\SoundMetrics.Aris.AcousticSettings.UT\SoundMetrics.Aris.AcousticSettings.UT.fsproj
 dotnet restore common\platform-dotnet\SoundMetrics.HID.Windows\SoundMetrics.HID.Windows.csproj
 dotnet restore common\platform-dotnet\SoundMetrics.HID.Windows.UT\SoundMetrics.HID.Windows.UT.csproj
 dotnet restore common\platform-dotnet\SoundMetrics.Data2\SoundMetrics.Data2.csproj
-dotnet restore common\platform-dotnet\test\WPF\WpfTestBench.ViewModel.fsproj
+dotnet restore common\platform-dotnet\WPF\WpfTestBench.Model\WpfTestBench.Model.fsproj
+dotnet restore common\platform-dotnet\WPF\WpfTestBench.ViewModel\WpfTestBench.ViewModel.fsproj
+dotnet restore common\platform-dotnet\Samples\ExtractRecordingInfo\ExtractRecordingInfo.csproj
