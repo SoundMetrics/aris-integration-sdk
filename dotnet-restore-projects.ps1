40b7033f88cb865488bbfc255e8da873435da6e3
Curt Nichols <curt.nichols@live.com>
2022-05-27 10:12:22 -0700

    Build fixes: additional projects to restore

--- dotnet-restore-projects.ps1
+++ dotnet-restore-projects.ps1
@@ -2,8 +2,11 @@
 
 dotnet restore submodules\build-tools\BuildInfo\BuildInfo.fsproj
 
+dotnet restore common\platform-dotnet\SoundMetrics.Aris\SoundMetrics.Aris.csproj
 dotnet restore common\platform-dotnet\SoundMetrics.Aris.Messages\SoundMetrics.Aris.Messages.csproj
 dotnet restore common\platform-dotnet\SoundMetrics.Aris.Comms\SoundMetrics.Aris.Comms.fsproj
+dotnet restore common\platform-dotnet\test\SoundMetrics.Aris.Comms.Integration.Test\SoundMetrics.Aris.Comms.Integration.Test.fsproj
+dotnet restore common\platform-dotnet\test\SoundMetrics.Aris.Comms.UT\SoundMetrics.Aris.Comms.UT.fsproj
 dotnet restore common\platform-dotnet\SoundMetrics.Aris.FrameHeaderInjection\SoundMetrics.Aris.FrameHeaderInjection.fsproj
 dotnet restore common\platform-dotnet\SoundMetrics.NativeMemory\SoundMetrics.NativeMemory.fsproj
 dotnet restore common\platform-dotnet\SoundMetrics.Aris.ReorderCS\SoundMetrics.Aris.ReorderCS.csproj
@@ -12,3 +15,9 @@ dotnet restore common\platform-dotnet\SoundMetrics.Common\SoundMetrics.Common.fs
 dotnet restore common\platform-dotnet\SoundMetrics.Network\SoundMetrics.Network.fsproj
 dotnet restore common\platform-dotnet\SoundMetrics.Dataflow\SoundMetrics.Dataflow.fsproj
 dotnet restore common\platform-dotnet\SoundMetrics.Data\SoundMetrics.Data.fsproj
+dotnet restore common\platform-dotnet\SsdpAdHocTest.Model\SsdpAdHocTest.Model.fsproj
+dotnet restore common\platform-dotnet\SoundMetrics.Aris.AcousticSettings.UT\SoundMetrics.Aris.AcousticSettings.UT.fsproj
+dotnet restore common\platform-dotnet\SoundMetrics.HID.Windows\SoundMetrics.HID.Windows.csproj
+dotnet restore common\platform-dotnet\SoundMetrics.HID.Windows.UT\SoundMetrics.HID.Windows.UT.csproj
+dotnet restore common\platform-dotnet\SoundMetrics.Data2\SoundMetrics.Data2.csproj
+dotnet restore common\platform-dotnet\test\WPF\WpfTestBench.ViewModel.fsproj
