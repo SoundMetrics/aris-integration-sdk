8e0c950d07518214ce3c55dbc150353faa3c4fdb
Curt Nichols <curt.nichols@live.com>
2023-05-30 08:37:09 -0700

    Refactor option types

---
+++ common/platform-dotnet/Samples/ExtractRecordingInfo/DepthOptions.cs
@@ -0,0 +1,9 @@
+﻿using CommandLine;
+
+namespace ExtractRecordingInfo
+{
+    [Verb("depth", HelpText = "Extracts depth information from the recording")]
+    public sealed class DepthOptions
+    {
+    }
+}
--- common/platform-dotnet/Samples/ExtractRecordingInfo/GpsOptions.cs
+++ common/platform-dotnet/Samples/ExtractRecordingInfo/GpsOptions.cs
@@ -3,17 +3,12 @@
 namespace ExtractRecordingInfo
 {
     [Verb("gps", HelpText = "Extracts GPS information from the recording")]
-    public class GpsOptions
+    public sealed class GpsOptions
     {
         [Option('s', "summary", Required = false, HelpText = "Summarize the file's GPS info state")]
         public bool Summary { get; set; }
 
         [Value(0, Required = true, MetaName = "Path")]
         public string Path { get; set; }
-    }
-
-    [Verb("depth", HelpText = "Extracts depth information from the recording")]
-    public class DepthOptions
-    {
     }
 }
