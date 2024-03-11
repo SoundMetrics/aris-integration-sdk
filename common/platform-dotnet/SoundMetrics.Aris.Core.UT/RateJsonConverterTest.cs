691ca5c17c35b99e42207f0597d648055940ae4b
Curt Nichols <curt.nichols@live.com>
2024-03-11 10:35:32 -0700

    Add RateJsonConverterTest

---
+++ common/platform-dotnet/SoundMetrics.Aris.Core.UT/RateJsonConverterTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System;
+
+namespace SoundMetrics.Aris.Core
+{
+    [TestClass]
+    public sealed class RateJsonConverterTest
+    {
+        [TestMethod]
+        public void RoundTripRate()
+        {
+            var original = new Rate(3, (FineDuration)5);
+            var expected = original;
+            var serialized = JsonConvert.SerializeObject(original);
+            var actual = JsonConvert.DeserializeObject<Rate>(serialized);
+
+            Console.WriteLine($"serialized=[{serialized}]");
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
