a201ac1c44a92b8f1605c4c68823fa580b978be4
Curt Nichols <curt.nichols@live.com>
2024-03-11 10:53:47 -0700

    Add Distance unit test for JSON serialization.

---
+++ common/platform-dotnet/SoundMetrics.Aris.Core.UT/DistanceJsonTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System;
+
+namespace SoundMetrics.Aris.Core
+{
+    [TestClass]
+    public sealed class DistanceJsonTest
+    {
+        [TestMethod]
+        public void RountTripDistance()
+        {
+            Distance original = Distance.FromMeters(1.25);
+            Distance expected = original;
+            string serialized = JsonConvert.SerializeObject(original);
+            Distance actual = JsonConvert.DeserializeObject<Distance>(serialized);
+
+            Console.WriteLine($"serialized=[{serialized}]");
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
