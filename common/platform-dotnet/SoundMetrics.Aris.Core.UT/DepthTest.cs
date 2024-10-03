632796b9332056f7cc240eb621c3bafa3b2e36f1
Curt Nichols <curt.nichols@live.com>
2024-10-03 10:01:54 -0500

    Add Depth.cs for depth calculation

---
+++ common/platform-dotnet/SoundMetrics.Aris.Core.UT/DepthTest.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SoundMetrics.Aris.Core
+{
+    using static Depth;
+
+    [TestClass]
+    public sealed class DepthTest
+    {
+        private const double Atmosphere = 14.6959;
+        private const double MetersPerPSI = 0.702398;
+
+        [TestMethod]
+        public void BrackishWaterDepthTest()
+        {
+            const double pressure = 32.0;
+            const float salinity = 15;
+            Temperature waterTemp = (Temperature)15;
+            const double brackishWaterDensity15C = 1.011f;
+            const double expected = (pressure - Atmosphere) * MetersPerPSI / brackishWaterDensity15C;
+            double actual = CalculateDepth(pressure, salinity, waterTemp).Meters;
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void SaltWaterDepthTest()
+        {
+            const double pressure = 65.0;
+            const float salinity = 35;
+            Temperature waterTemp = (Temperature)15;
+            const double saltWaterDensity15C = 1.026f;
+            const double expected = (pressure - Atmosphere) * MetersPerPSI / saltWaterDensity15C;
+            double actual = CalculateDepth(pressure, salinity, waterTemp).Meters;
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod] 
+        public void WarmWaterDepthTest()
+        {
+            const double pressure = 23.0;
+            const float salinity = 35;
+            Temperature waterTemp = (Temperature)40;
+            const double saltWaterDensity30C = 1.022f;
+            const double expected = (pressure - Atmosphere) * MetersPerPSI / saltWaterDensity30C;
+            double actual = CalculateDepth(pressure, salinity, waterTemp).Meters;
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        // salinity of 50 PPT is invalid so treat as salt water
+        [TestMethod]
+        public void WrongWaterDepthTest()
+        {
+            const double pressure = 37.0;
+            const float salinity = 50;
+            Temperature waterTemp = (Temperature)10;
+            const double saltWaterDensity10C = 1.027f;
+            const double expected = (pressure - Atmosphere) * MetersPerPSI / saltWaterDensity10C;
+            double actual = CalculateDepth(pressure, salinity, waterTemp).Meters;
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void LowTempDepthTest()
+        {
+          const double pressure = 65.0;
+          const float salinity = 35;
+          Temperature waterTemp = (Temperature)(-2);
+          const double saltWaterDensity0C = 1.028f;
+          const double expected = (pressure - Atmosphere) * MetersPerPSI / saltWaterDensity0C;
+          double actual = CalculateDepth(pressure, salinity, waterTemp).Meters;
+
+          Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void HighTempDepthTest()
+        {
+          const double pressure = 65.0;
+          const float salinity = 35;
+          Temperature waterTemp = (Temperature)31;
+          const double saltWaterDensity30C = 1.022f;
+          const double expected = (pressure - Atmosphere) * MetersPerPSI / saltWaterDensity30C;
+          double actual = CalculateDepth(pressure, salinity, waterTemp).Meters;
+
+          Assert.AreEqual(actual, expected);
+        }
+    }
+}
---
+++ common/platform-dotnet/SoundMetrics.Aris.Core/Depth.cs
@@ -0,0 +1,110 @@
+﻿// Copyright 2014-2024 Sound Metrics corporation
+
+namespace SoundMetrics.Aris.Core;
+
+using System;
+using System.Diagnostics;
+
+using static System.Math;
+using static Temperature;
+
+public static class Depth
+{
+    public static Distance CalculateDepth(
+        double pressurePSI,
+        float salinityPPT,
+        Temperature temperature)
+    {
+        double cf = GetConversionFactor(salinityPPT, temperature);
+
+        double value = (pressurePSI - 14.6959) * 0.702398 / cf;
+        return (Distance)value;
+    }
+
+
+    private static int TemperatureToIndex(
+        in ReadOnlySpan<float> cfs, 
+        Temperature temperature) 
+    {
+        int maxIdx = cfs.Length - 1;
+
+        // The temperature is the index into cfs. There are 5-degree ranges for each, so
+        // idx = lround(temp / 5).
+
+        temperature = Max(Zero, temperature); // Bottom of the scale is zero.
+
+        int idx = (int)Round((temperature / 5).DegreesCelsius); // wraps to really big index on negative
+        return Min(maxIdx, idx);
+    }
+
+    private static ReadOnlySpan<float> SelectCFRange(float salinityPPT)
+    {
+
+        if (salinityPPT >= 35)
+        {
+            return tempToSeaDepthCFs.Span;
+        }
+        else if (salinityPPT >= 15)
+        {
+            return tempToBrackishDepthCFs.Span;
+        }
+        else
+        {
+            Debug.Assert(salinityPPT >= 0);
+            return tempToFreshDepthCFs.Span;
+        }
+    }
+
+    private static float SelectCF(
+        in ReadOnlySpan<float> cfs,
+        Temperature temperature)
+    {
+
+        int idx = TemperatureToIndex(cfs, temperature);
+        return cfs[idx];
+    }
+
+    private static double GetConversionFactor(
+        float salinityPPT, 
+        Temperature temperature)
+    {
+        return SelectCF(SelectCFRange(salinityPPT), temperature);
+    }
+
+    // 3 sets of conversion factors for 3 different salinities: fresh, brackish and sea water
+    // The implied key is water temperature in Celsius.
+    // The value is water density.
+
+    private static readonly ReadOnlyMemory<float> tempToFreshDepthCFs = new[] 
+    {
+        1.000f, // 0C
+        1.000f, // 5C
+        1.000f, // 10C
+        0.999f, // 15C
+        0.998f, // 20C
+        0.997f, // 25C
+        0.996f, // 30C
+    };
+
+    private static readonly ReadOnlyMemory<float> tempToBrackishDepthCFs = new[]
+    {
+        1.012f, // 0C
+        1.012f, // 5C
+        1.011f, // 10C
+        1.011f, // 15C
+        1.010f, // 20C
+        1.008f, // 25C
+        1.007f, // 30C
+      };
+
+    private static readonly ReadOnlyMemory<float> tempToSeaDepthCFs = new[]
+    {
+        1.028f, // 0C
+        1.028f, // 5C
+        1.027f, // 10C
+        1.026f, // 15C
+        1.025f, // 20C
+        1.023f, // 25C
+        1.022f, // 30C
+      };
+}
