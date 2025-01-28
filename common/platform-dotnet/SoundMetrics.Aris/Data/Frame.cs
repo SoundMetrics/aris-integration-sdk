dc6a906e716fee1a0d886bd9616fb6f1db531736
Curt Nichols <curt.nichols@live.com>
2025-01-28 14:55:03 -0600

    Switch Frame to a record class

--- common/platform-dotnet/SoundMetrics.Aris/Data/Frame.cs
+++ common/platform-dotnet/SoundMetrics.Aris/Data/Frame.cs
@@ -3,24 +3,25 @@ using System;
 
 namespace SoundMetrics.Aris.Data
 {
-    public sealed class Frame
+    public record class Frame(
+        FrameHeader FrameHeader,
+        SampleGeometry SampleGeometry,
+        ReadOnlySampleBuffer Samples)
     {
         public static Frame Create(in FrameHeader frameHeader, ReadOnlySampleBuffer samples)
         {
-            if (samples is null)
-            {
-                throw new ArgumentNullException(nameof(samples));
-            }
+            ArgumentNullException.ThrowIfNull(samples);
 
-            if (SystemConfiguration.TryGetSampleGeometry(frameHeader, out var sampleGeometry))
+            if (SystemConfiguration.TryGetSampleGeometry(frameHeader, out var sampleGeometry)
+                && sampleGeometry is SampleGeometry sg)
             {
-                if (sampleGeometry!.TotalSampleCount != samples.Length)
+                if (sg.TotalSampleCount != samples.Length)
                 {
                     throw new ArgumentException(
-                        $"Sample count doesn't match frame header; expected [{sampleGeometry.TotalSampleCount}], found [{samples.Length}]");
+                        $"Sample count doesn't match frame header; expected [{sg.TotalSampleCount}], found [{samples.Length}]");
                 }
 
-                return new Frame(frameHeader, sampleGeometry, samples);
+                return new Frame(frameHeader, sg, samples);
             }
             else
             {
@@ -29,21 +30,5 @@ namespace SoundMetrics.Aris.Data
                 throw new ArgumentException(msg, nameof(frameHeader));
             }
         }
-
-        private Frame(
-            in FrameHeader frameHeader,
-            in SampleGeometry sampleGeometry,
-            ReadOnlySampleBuffer samples)
-        {
-            FrameHeader = frameHeader;
-            SampleGeometry = sampleGeometry;
-            Samples = samples;
-        }
-
-        public FrameHeader FrameHeader { get; private set; }
-
-        public ReadOnlySampleBuffer Samples { get; private set; }
-
-        public SampleGeometry SampleGeometry { get; private set; }
     }
 }
