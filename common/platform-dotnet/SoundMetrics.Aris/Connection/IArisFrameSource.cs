f0dd5bd1e41112693a7109477d1323ce4d6638e0
Curt Nichols <curt.nichols@live.com>
2025-07-01 09:37:49 -0500

    New interface IArisFrameSource for managing connections

---
+++ common/platform-dotnet/SoundMetrics.Aris/Connection/IArisFrameSource.cs
@@ -0,0 +1,11 @@
+﻿using SoundMetrics.Aris.Data;
+using System;
+
+namespace SoundMetrics.Aris.Connection;
+
+public interface IArisFrameSource
+{
+    IObservable<Frame> Frames { get; }
+
+    uint SerialNumber { get; }
+}
